a32df01acf364ce2ea8dc856fe455462e5e9bbe8
Ray Ortigas <ray.ortigas@gmail.com>
2024-11-17 20:51:02 +0000

    Update training script to use LoRA if supplied.

--- sst2.py
+++ sst2.py
@@ -1,28 +1,41 @@
+import argparse
+import logging
 from typing import Dict, List
 
 import evaluate
 import numpy as np
 import torch
 from datasets import load_dataset
-from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
-                          DataCollatorWithPadding, Trainer, TrainingArguments)
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+)
 
-from lora import LoRALayer, LoRAUpdate
+import lora
+
+logger = logging.getLogger(__name__)
 
 
+def train_sst2(args: argparse.Namespace) -> None:
+    filename = f"sst2-{args.mode}.pt"
+    logger.info(f"will train model and save to {filename}")
 
-def train_sst2():
     sst2 = load_dataset("stanfordnlp/sst2")
 
     tokenizer = AutoTokenizer.from_pretrained("google/mobilebert-uncased")
+
     def tokenize(batch: Dict[str, List]):
         return tokenizer(batch["sentence"], truncation=True, max_length=512)
-    
+
     tokenized_sst2 = sst2.map(tokenize, batched=True)
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     accuracy = evaluate.load("accuracy")
+
     def compute_metrics(eval_prediction):
         predictions, labels = eval_prediction
         return accuracy.compute(
@@ -35,11 +48,21 @@ def train_sst2():
         num_labels=2,
     )
 
+    match args.mode:
+        case "lora":
+            lora.wrap_bert_model_with_lora(
+                model, "mobilebert", rank=args.lora_rank, alpha=args.lora_rank
+            )
+
+    num_trainable_parameters = sum([param.numel() for param in model.parameters() if param.requires_grad])
+    logger.info(f"# trainable parameters: {num_trainable_parameters}")
+
     model = model.to("cuda")
 
     training_args = TrainingArguments(
-        output_dir="tmp/mobilebert",
-        learning_rate=2e-5,
+        output_dir=f"tmp/sst2-{args.mode}",
+        learning_rate=args.learning_rate,
+        warmup_ratio=args.warmup_ratio,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=5,
@@ -62,7 +85,42 @@ def train_sst2():
 
     trainer.train()
 
-    torch.save(model.state_dict(), "base-sst2.pt")
+    torch.save(model.state_dict(), f"sst2-{args.mode}.pt")
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mode",
+        type=str,
+        required=True,
+        choices=["base", "lora"],
+        default="base",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        required=True,
+    )
+    parser.add_argument(
+        "--warmup_ratio",
+        type=float,
+        required=False,
+    )
+    parser.add_argument(
+        "--lora_rank",
+        type=int,
+        required=False,
+    )
+    args = parser.parse_args()
+    if args.mode == "lora":
+        if not args.lora_rank:
+            parser.error("`lora_rank` is required if using `lora` mode.")
+
+    return args
+
 
 if __name__ == "__main__":
-    train_sst2()
+    logging.basicConfig(level=logging.INFO)
+    args = get_args()
+    train_sst2(args)
