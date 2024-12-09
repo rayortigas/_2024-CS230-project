0a7fadd8ae5e31e63f9e0914834ad0bec09ecca0
Ray Ortigas <ray.ortigas@gmail.com>
2024-12-09 17:10:02 +0000

    Shortcut to avoid invoking teacher model if lambda = 0.

--- distillation.py
+++ distillation.py
@@ -21,20 +21,25 @@ class DistillationTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         student_outputs = model(**inputs)
 
-        # Given how we're working with labeled datasets in Hugging Face, it'll automatically return cross-entropy loss
-        # in the student's outputs for us.
-        # https://huggingface.co/docs/transformers/main_classes/output#model-outputs
-        # For KL loss, we need the logits from the student's outputs and the teacher's outputs.
-        student_loss = student_outputs.loss
-        student_logits = student_outputs.logits
-        with torch.no_grad():
-            teacher_outputs = self.teacher_model(**inputs)
-            teacher_logits = teacher_outputs.logits
-        kl_loss = self.kl_loss(
-            F.log_softmax(student_logits / self.args.temperature, dim=-1),
-            F.softmax(teacher_logits / self.args.temperature, dim=-1),
-        )
-        loss = (
-            1.0 - self.args.lambd
-        ) * student_loss + self.args.lambd * self.args.temperature**2.0 * kl_loss
+        # If lambda is zero, focus on student CE loss entirely to save teacher calculations and training time.
+        # Otherwise, calculate KL loss as well.
+        if self.args.lambd <= 0.0:
+            # Given how we're working with labeled datasets in Hugging Face, it'll automatically return cross-entropy loss
+            # in the student's outputs for us.
+            # https://huggingface.co/docs/transformers/main_classes/output#model-outputs
+            loss = student_outputs.loss
+        else:
+            # For KL loss, we need the logits from the student's outputs and the teacher's outputs.
+            student_logits = student_outputs.logits
+            with torch.no_grad():
+                teacher_outputs = self.teacher_model(**inputs)
+                teacher_logits = teacher_outputs.logits
+            kl_loss = self.kl_loss(
+                F.log_softmax(student_logits / self.args.temperature, dim=-1),
+                F.softmax(teacher_logits / self.args.temperature, dim=-1),
+            )
+            loss = (
+                (1.0 - self.args.lambd) * student_outputs.loss
+                + self.args.lambd * self.args.temperature**2.0 * kl_loss
+            )
         return (loss, student_outputs) if return_outputs else loss
